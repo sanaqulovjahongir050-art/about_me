563b8e7991fa01fc7f7e58dbfcf7570de233bbc5
Jahongir Sanaqulov <sanaqulovjahongir050@gmail.com>
2026-02-22 20:51:13 +0500

    Add about_me.py file

---
+++ about_me.py
@@ -0,0 +1,10 @@
+ism="Jahongir"
+familiya="Sanaqulov"
+yosh=18
+vatan="o'zbekiston"
+university="akhu"
+print(f"mening ismim {ism}.")
+print(f"mening familiyam {familiya}.")
+print(f"i am {yosh}.")
+print(f'i live in {vatan}.')
+print(f"I study in {university}.")
